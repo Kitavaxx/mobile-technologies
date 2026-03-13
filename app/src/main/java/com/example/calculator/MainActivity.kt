b51fcb295afaee38e0e150356f8b1cb0a7cac092
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-03-13 20:51:42 +0100

    Implement UI for calculator menu

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.calculator
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -16,10 +16,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.calculator.ui.theme.MycalculatorTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -27,35 +25,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MycalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                content = { padding ->
+                    CalculatorMenu(
+                        modifier = Modifier.padding(padding),
+
+                        { println("Basic clicked") },
+                        { println("Basic clicked") },
+                        { println("Basic clicked") },
+                        { println("Basic clicked") }
                     )
                 }
-            }
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun CalculatorMenu(
+    modifier: Modifier = Modifier,
+    
     onBasicClick: () -> Unit,
     onAdvancedClick: () -> Unit,
     onAboutClick: () -> Unit,
     onExitClick: () -> Unit
 ) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .padding(24.dp),
         verticalArrangement = Arrangement.Center,
@@ -79,7 +76,7 @@ fun MenuButton(text: String, onClick: () -> Unit){
     Button(
         onClick = onClick,
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .padding(10.dp)
             .height(60.dp)
     ){
@@ -98,12 +95,4 @@ fun CalculatorUI() {
 @Composable
 fun CalculatorButton(label: String, onClick: (String) -> Unit) {
 
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MycalculatorTheme {
-        Greeting("Android")
-    }
 }
