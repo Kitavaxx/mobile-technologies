63c82fe4769b2e15bb7bb98c76550db5fc1eab70
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-03-14 00:07:39 +0100

    Add about page

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.calculator
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -130,8 +135,32 @@ fun AdvancedCalculatorUI(navController : NavController) {
 }
 @Composable
 fun AboutPageUI(navController : NavController) {
-    MenuButton("Back"){
-        navController.popBackStack()
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Text(
+            text = "Calculator App\n\n",
+            fontSize = 34.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text =  "Made by: Dmytro Malinovskyi - 252501\n\n\n" +
+                    "This calculator app combines both basic and advanced functionality in one place " +
+                    "It handles standard arithmetic operations like addition, subtraction, multiplication, and " +
+                    "division as well as scientific functions such as square roots," +
+                    " exponents, trigonometry, and logarithms—perfect for everyday calculations and " +
+                    "more complex math needs.",
+            fontSize = 20.sp,
+        )
+        Spacer(modifier = Modifier.size(60.dp))
+        MenuButton("Back"){
+            navController.popBackStack()
+        }
     }
 }
 
