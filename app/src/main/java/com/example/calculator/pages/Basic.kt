68ea3087e99fc32c43654f6438ad7aace5417763
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-04-02 19:17:57 +0200

    implement scroll for both of calculators

--- app/src/main/java/com/example/calculator/pages/Basic.kt
+++ app/src/main/java/com/example/calculator/pages/Basic.kt
@@ -4,18 +4,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.calculator.buttons.CalculatorButton
@@ -27,6 +32,11 @@ fun BasicCalculator() {
     val viewModel : MyViewModel = viewModel()
     val input = viewModel.input.collectAsState()
     val currentContext = LocalContext.current
+    val scrollState = rememberScrollState()
+
+    LaunchedEffect(input) {
+        scrollState.scrollTo(scrollState.maxValue)
+    }
 
     Column(
         modifier = Modifier.fillMaxSize()
@@ -35,7 +45,9 @@ fun BasicCalculator() {
 
         TextField(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .height(200.dp)
+                .verticalScroll(scrollState),
             value = input.value,
             onValueChange = { viewModel.onInputChange(it) },
             textStyle = TextStyle(fontSize = 48.sp, textAlign = TextAlign.End),
